2e8f1769939f511b7743e93ab4ad94543a5ca127
Alvaro Viebrantz <alvarowolfx@gmail.com>
2022-05-19 12:17:47 -0400

    fix: remove credentials

--- examples/magtag/secrets.h
+++ examples/magtag/secrets.h
@@ -1,9 +1,9 @@
 #ifndef secrets_h
 #define secrets_h
 
-#define WIFI_SSID "FullStackIoT"
-#define WIFI_PASS "FullStackIoT"
-#define PSK_ID "alvaro@project-1"
-#define PSK "decda859b4fa8a7de82ece1a29ecb33e"
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+#define PSK_ID ""
+#define PSK ""
 
 #endif // secrets_h
